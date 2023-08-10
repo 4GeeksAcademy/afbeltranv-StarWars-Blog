38e0ee224b50da394cad316b53bd423a96bc197b
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-08-10 02:22:54 +0000

    Primer commit, map realizado básico y elemento card ta,bién generado

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,33 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useEffect, useState} from "react";
 import "../../styles/home.css";
+import { Card } from "../component/card";
 
-export const Home = () => (
+export const Home = () => {
+	
+	const [characters, setCharacters]=useState([]); 
+
+	function getCharacters(){
+		console.log("acaaa se cargó la vaina")
+	}
+
+	useEffect(()=>{
+		getCharacters();
+		fetch('https://www.swapi.tech/api/people')
+		.then((response)=>response.json())
+		// .then((data)=>console.log(data.results))
+		.then((data)=>setCharacters(data.results))
+
+	}, [])
+	
+	return(
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<h1>Home!</h1>
+		{characters.map((character)=><p>{character.name}</p>)}
+
+
+
+		<Card name="desde home"/>
+		<Card name="Awa"/>
+		
 	</div>
-);
+)};
